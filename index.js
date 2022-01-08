97fd921a431e2d121a689e124685b6bb8fe4ec4e
Jerry Zhu <jerryzhu99@gmail.com>
2022-01-07 17:42:48 -0800

    Add logging and stats

--- index.js
+++ index.js
@@ -3,20 +3,39 @@ const nsfw = require("nsfwjs");
 const path = require("path");
 const { promises: fs } = require("fs");
 
-
-
 const htmlTemplate = (template, results) => {
-    return template.replace('{{results}}', results.map(e => `
-    <a href="file://${e.image}">
-      <div class="grid-item">
-        <div class="rating">${(100 * e.result.Hentai).toFixed(2)}%</div>
-        <img src="file://${e.image}" loading="lazy" />
-      </div>
-    </a>`).join(''));
-}
-
+  const count = results.filter(
+    (e) => e.result.Hentai >= Math.max(...Object.values(e.result))
+  ).length;
+  const total = results.length;
+  const percentage = ((100 * count) / total).toFixed(2);
+  return template
+    .replace(
+      "{{stats}}",
+      `Found ${count}/${total} (${percentage}%) images classified as hentai.`
+    )
+    .replace(
+      "{{results}}",
+      results
+        .map(
+          (e) => `
+            <a href="file://${e.image.replaceAll("#", "%23")}">
+              <div class="grid-item">
+                <div class="rating">${(100 * e.result.Hentai).toFixed(2)}%</div>
+                <img src="file://${e.image.replaceAll(
+                  "#",
+                  "%23"
+                )}" loading="lazy" />
+              </div>
+            </a>`
+        )
+        .join("")
+    );
+};
 
 async function fn() {
+  console.log("Starting osucheck...");
+
   const rootDir = process.argv[2] ?? process.cwd();
   const songsDir = path.resolve(rootDir, "Songs");
 
@@ -25,6 +44,8 @@ async function fn() {
     withFileTypes: true,
   });
   let osuFiles = [];
+
+  console.log("Searching beatmaps...");
 
   for (let dir of mapDirs) {
     if (!dir.isDirectory()) continue;
@@ -56,7 +77,10 @@ async function fn() {
 
   console.log(`Found ${imageFiles.size} images`);
 
-  const model = await nsfw.load(`file://${path.resolve(__dirname, 'nsfwjs')}/`, { size: 299 });
+  const model = await nsfw.load(
+    `file://${path.resolve(__dirname, "nsfwjs")}/`,
+    { size: 299 }
+  );
 
   const results = [];
   let i = 0;
@@ -65,7 +89,7 @@ async function fn() {
     console.log(`Processing image ${i} of ${imageFiles.size}`);
     try {
       const pic = await fs.readFile(imageFile);
-      const image = await tf.node.decodeImage(pic, 3);
+      const image = tf.node.decodeImage(pic, 3);
       const predictions = await model.classify(image);
       image.dispose();
       const result = {
@@ -75,18 +99,21 @@ async function fn() {
         ),
       };
       results.push(result);
-    } catch (e) {
-      console.error(e);
-    }
-    // if (i > 100) break; // TODO: Remove
+    } catch (e) {}
   }
 
   results.sort((a, b) => b.result.Hentai - a.result.Hentai);
-  console.log(results.map(e => `${(100 * e.result.Hentai).toFixed(2)}%: ${e.image}`).join('\n'));
+  console.log(
+    results
+      .map((e) => `${(100 * e.result.Hentai).toFixed(2)}%: ${e.image}`)
+      .join("\n")
+  );
 
-  const template = await fs.readFile(path.resolve(__dirname, 'index.html'), {encoding:'utf-8'});
+  const template = await fs.readFile(path.resolve(__dirname, "index.html"), {
+    encoding: "utf-8",
+  });
   const output = htmlTemplate(template, results);
 
-  fs.writeFile('results.html', output);
+  fs.writeFile("results.html", output);
 }
 fn();
